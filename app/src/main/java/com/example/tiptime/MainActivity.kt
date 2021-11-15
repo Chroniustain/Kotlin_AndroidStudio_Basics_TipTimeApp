1bf1f352c66a6180c2c107cb4841844f754d0a15
Chroniustain <93010262+Chroniustain@users.noreply.github.com>
2021-11-16 00:32:29 +0200

    "Fix bug (app crash after empty cost field)" "Fix visual bug of tip amount staying in screen"

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -20,7 +20,11 @@ class MainActivity : AppCompatActivity() {
     fun calculateTip(){
 
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text ="Tip Amount: "
+            return
+        }
 
         val selectedId = binding.tipOptions.checkedRadioButtonId
         val tipPercentage = when (selectedId) {
